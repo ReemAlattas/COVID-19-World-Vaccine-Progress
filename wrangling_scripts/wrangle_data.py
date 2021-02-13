8a614d0c9d6ca9b1f6bb2809c1033f7707ef7ac6
Reem <ralattas@live.com>
2021-02-13 17:50:07 +0300

    Define function headers

--- wrangling_scripts/wrangle_data.py
+++ wrangling_scripts/wrangle_data.py
@@ -1,1 +1,31 @@
+import pandas as pd
+import plotly.graph_objs as go
 
+def cleandata(dataset):
+    """Clean world vaccination data for a visualizaiton dashboard
+
+
+
+    Args:
+        dataset (str): name of the csv data file
+
+    Returns:
+        clean data frame
+
+    """    
+    
+    return df
+  
+  
+def return_figures():
+    """Creates four plotly visualizations
+
+    Args:
+        None
+
+    Returns:
+        list (dict): list containing the four plotly visualizations
+
+    """
+    
+    return figures
